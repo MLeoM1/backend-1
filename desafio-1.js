c5353aad2aade1ed9b663cdff1a596395bbb5584
MLeoM1 <mleofv1@gmail.com>
2023-02-27 16:32:26 -0300

    re-entrega

--- desafio-1.js
+++ desafio-1.js
@@ -1,52 +1,67 @@
-class Product {
-    constructor(id, title, description, price, thumbnail, code, stock) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-    }
-}
-
 class ProductManager {
-    constructor() {
-        this.products = [];
-        this.lastProductId = 0;
-    }
+  constructor() {
+    this.products = [];
+    this.lastId = 0;
+  }
 
-    // Método para agregar un producto
-    addProduct(title, description, price, thumbnail, code, stock) {
-      // Validar que los campos obligatorios no estén vacíos
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-        throw new Error("Todos los campos son obligatorios");
-    }
+  getProducts() {
+    return this.products;
+  }
 
-      // Validar que el campo "code" no se repita
-    const productWithSameCode = this.products.find((p) => p.code === code);
-    if (productWithSameCode) {
-        throw new Error("Ya existe un producto con el mismo código");
-    }
-
-      // Asignar un id autoincrementable al producto
-    const id = ++this.lastProductId;
-
-      // Retornar el producto agregado
-    return product;
-    }
-
-    // Método para obtener todos los productos
-    getProducts() {
-        return this.products;
-    }
-
-    // Método para obtener un producto por ID
-    getProductById(id) {
+  getProductById(id) {
     const product = this.products.find((p) => p.id === id);
     if (!product) {
-        console.error("Not found");
+      throw new Error(`Producto no encontrado con ID ${id}`);
     }
     return product;
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    if (this.products.some((p) => p.code === code)) {
+      throw new Error(`El producto con el codigo ${code} ya existe`);
     }
+    const product = {
+      id: ++this.lastId,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+    this.products.push(product);
+    return product;
+  }
 }
+
+const productManager = new ProductManager();
+
+console.log(productManager.getProducts()); 
+
+const newProduct = productManager.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "Sin imagen",
+  "abc123",
+  25
+);
+console.log(newProduct); 
+
+console.log(productManager.getProducts()); 
+
+try {
+  productManager.addProduct(
+    "otro producto",
+    "Este es otro producto",
+    150,
+    "Sin imagen",
+    "abc123",
+    20
+  );
+} catch (error) {
+  console.log(error.message);
+}
+
+const productById = productManager.getProductById(1);
+console.log(productById); 
